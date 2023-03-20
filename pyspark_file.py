8b104122d2717db890f7742f78a66f96aa749237
Gayu168 <gaythri.n@ctgdatabit.com>
2023-03-20 20:04:13 +0530

    Facing some issue on pyspark.pandas.concatenate method to excel sheet

--- pyspark_file.py
+++ pyspark_file.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 import mysql.connector
 import pandas as pd
 #import pyarrow as pq
+import pyspark.pandas as ps
+import pyspark.pandas as ps
+ps.set_option('compute.ops_on_diff_frames', True)
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 pd.options.display.width = 1000
@@ -82,14 +85,15 @@ def compare_fun(source,target):
 
          """  Getting  column name of source and target from user and concatenate with target """
 
-         #s_eq=source[source_col].eq(target[target_col]).rename(f's_{source_col}')
-         #t_eq = target[target_col].eq(source[source_col]).rename(f't_{source_col}')
-         s_col = source.select(col(source_col)) # .rename(f's_{source_col}')
-         t_col = target.select(col(target_col))  # .rename(f't_{source_col}')
-         s_eq = s_col.eq(t_col)
-         t_eq = t_col.eq(s_col)
-         df = s_eq.concat(t_eq,axis = 1)
-         print(df)
+         s_eq = source[source_col].eq(target[target_col]).rename(f's_{source_col}')
+         t_eq = target[target_col].eq(source[source_col]).rename(f't_{source_col}')
+         #s_col = source.select(col(source_col)) # .rename(f's_{source_col}')
+         #t_col = target.select(col(target_col))  # .rename(f't_{source_col}')
+        ## s_eq = s_col.eq(t_col)
+        # t_eq = t_col.eq(s_col)
+         #df = ps.concat([s_eq,t_eq],axis = 1)
+         df = ps.concat([s_eq,t_eq], axis=1)
+         #print(df)
          return df
 
     def color_mismatch(val):
@@ -104,31 +108,41 @@ def compare_fun(source,target):
 
         """ Columns are arranged together in a single Dataframe"""
 
-        #s_col = source.withColumnRenamed(col(source_col),col(f's_{source_col}'))
-        #t_col = target.withColumnRenamed(col(target_col),col(f't_{source_col}'))
-        s_col = source.select(col(source_col))
-        t_col = target.select(col(target_col))
-        print(type(s_col))
-        df = s_col.concat(t_col,axis=1)
+        s_col = source[source_col].rename(f's_{source_col}')
+        print(s_col)
+        #s_col = ps.series
+        type(s_col)
+        t_col =target[target_col].rename(f't_{source_col}')
+       # t_col = ps.series
+        df = ps.concat([s_col, t_col], axis=1)
         return df
 
-    bool_df = pd.DataFrame()
-    original_df = pd.DataFrame()
+        #s_col = source.withColumnRenamed(col(source_col),col(f's_{source_col}'))
+        #t_col = target.withColumnRenamed(col(target_col),col(f't_{source_col}'))
+       # s_col = source.select(col(source_col))
+        #t_col = target.select(col(target_col))
+        #print(type(s_col))
+        #df = s_col.concat(t_col,axis=1)
+       # return df
+
+    b_df = ps.DataFrame()
+    original_df = ps.DataFrame()
     for _ in range(3):                ###len(tuple(zip(source,target)))):
         source_col = input('Choose the source col:')
         target_col = input('Choose the target col:')
         concat_col_= concat_col(source_col,target_col)
-        original_df = pd.concat([original_df,concat_col_],axis = 1)
+        original_df = ps.concat([original_df,concat_col_],axis = 1)
         mask_= mask(source_col,target_col)
-        bool_df = pd.concat([bool_df,mask_],axis = 1)
+        b_df = ps.concat([b_df,mask_],axis = 1)
         #colur_=original_df.style.apply(lambda x:bool_df.applymap(color_mismatch),axis = None).to_excel("Output.xlsx",index=False)
 
-    bool_df1 = bool_df[(bool_df == False).any(axis=1)]
-    updated_df = original_df.iloc[bool_df1.index, :]
-    print(bool_df1)
+    b_df1 = b_df[(b_df == False).any(axis=0)]
+    print(b_df1)
+    updated_df = original_df.iloc[b_df1.index]
+    print(b_df1)
     print(updated_df)
     try:
-       updated_df.style.apply(lambda x: bool_df1.applymap(color_mismatch), axis=None).to_excel("excelfile.xlsx",index=False)
+       updated_df.style.apply(lambda x: b_df1.applymap(color_mismatch), axis=None).to_excel("excelfile.xlsx",index=False)
 
     except PermissionError:
         print('File permission denied')
@@ -163,7 +177,7 @@ class FileConnection:
     def read_csv(self, file_path: str) -> pd.DataFrame:
         try:
 
-            df = spark.read.csv(file_path, header=True)
+            df = ps.read_csv(file_path)
             return df
 
         except Exception as e:
@@ -241,8 +255,8 @@ def get_type_file(type: str) -> dict:
          source_ = file_con.create_dataframe(file_path1=path1, file_type1=file_type)
         # source_profiling = json_profiling(source_)
         # print(source_profiling)
-         source_schema = get_schema_flatfile(source_)
-         print("Source Database schema", source_schema)
+        # source_schema = get_schema_flatfile(source_)
+         #print("Source Database schema", source_schema)
          return source_
 
 
@@ -259,8 +273,8 @@ def get_type_file(type: str) -> dict:
          return sort_sr
 
 if __name__ == '__main__':
-    sc = SparkContext(master="local", appName="spark_demo")
-    spark = SparkSession.builder.appName("SparkByExamples.com").getOrCreate()
+    #sc = SparkContext(master="local", appName="spark_demo")
+   # spark = SparkSession.builder.appName("SparkByExamples.com").getOrCreate()
     src_type = input("Enter the type of file: 1-Flatfile  2-Database :")
     #st = time.time()
     source = get_type_file(src_type)
